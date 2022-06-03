205b99889cd4347b0ee9dde9eb8b1d07eca7466b
Fatih <curaat79@gmail.com>
2022-06-03 15:09:04 +0300

    initial project structure

---
+++ src/main/java/com/patika/emlakjet/Week2AssignmentFatihbuyukgucluApplication.java
@@ -0,0 +1,13 @@
+package com.patika.emlakjet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2AssignmentFatihbuyukgucluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2AssignmentFatihbuyukgucluApplication.class, args);
+    }
+
+}
